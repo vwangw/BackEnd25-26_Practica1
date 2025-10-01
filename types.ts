37527f1a7b06f6898e668ec451a3358b7269ad85
HugoThibaultM <hmarchandc@alumnos.nebrija.es>
2025-10-01 14:31:22 +0200

    tipos

---
+++ types.ts
@@ -0,0 +1,18 @@
+type Lugar = { 
+    id:number; 
+    nombre:string; 
+    tipo:string; 
+    peligro:number; 
+}; 
+type Personaje = {
+     id:number; 
+     nombre:string; 
+     fuerza:number; 
+     aliado:boolean; 
+}; 
+type Objeto = { 
+    id:number; 
+    nombre:string; 
+    poder:number; 
+    categoria:string; 
+};
